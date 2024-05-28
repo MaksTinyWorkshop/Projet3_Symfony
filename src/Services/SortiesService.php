2b86c4b02d79f47ceda4fe8f5ee38ca1539c5e5a
Shim0ji <shimoji@hotmail.fr>
2024-05-28 17:26:44 +0200

    Listes sorties en cours et archives

--- src/Services/SortiesService.php
+++ src/Services/SortiesService.php
@@ -1,6 +1,6 @@
 <?php
 
-////////////////// SORTIES SERVICES /////////////////////
+//////////////////// SORTIE SERVICE /////////////////////
 ///                                                   ///
 /// Sert à gérer les différents traitement liés au    ///
 /// ServiceController.                                ///
@@ -10,15 +10,28 @@
 
 namespace App\Services;
 
+use App\Entity\Sortie;
 use App\Repository\SortieRepository;
-use Doctrine\ORM\EntityManager;
-
 class SortiesService
 {
-    function showAll() : array
+    ////////////////////////////////////// les variables
+    private $sortieRepository;
+
+    ////////////////////////////////////// constructeur
+    public function __construct(SortieRepository $sortieRepository)
     {
-        $SR = SortieRepository::class;
-        $sorties = $SR->findAll();
+        $this->sortieRepository = $sortieRepository;
+    }
+
+    ////////////////////////////////////// les fonctions
+    public function showActive()   //recherche de toutes les sorties
+    {
+        $sorties = $this->sortieRepository->findBy([ 'etat' => [2,3,4,6] ]);
+        return $sorties;
+    }
+    public function showOld()   //recherche des sorties archivées
+    {
+        $sorties = $this->sortieRepository->findBy([ 'etat' => 5 ]);
         return $sorties;
     }
 }
