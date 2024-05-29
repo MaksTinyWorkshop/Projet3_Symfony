aaa06f3480fef6c0d6c40de4223800469eccf71c
HaroldB44360 <harold.bretonniere2023@campus-eni.fr>
2024-05-29 09:02:49 +0200

    register form + CSS

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,97 +3,47 @@
 namespace App\Form;
 
 use App\Entity\Participants;
-use App\Entity\Site;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\File;
 
-/**
- * Formulaire de création de compte
- */
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Email',
-            ])
-            ->add('nom', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Nom',
-            ])
-            ->add('prenom', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Prénom',
-            ])
-            ->add('telephone', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Téléphone',
-            ])
-            ->add('pseudo', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control'
-                ],
-                'label' => 'Pseudo',
-            ])
-            ->add('site', EntityType::class, [
-                'class' => Site::class,
-                'choice_label' => 'nom',
-                'placeholder' => 'Choisissez un site',
-            ])
+            ->add('prenom')
+
+            ->add('nom')
+
+            ->add('email')
+
             ->add('plainPassword', PasswordType::class, [
-                // lu et encodé dans le controller
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control'],
-
-                'label' => 'Mot de Passe',
-            ])
-            ->add('photo', FileType::class, [
-                'label' => 'Photo de profil (JPEG, PNG, GIF)',
-
-                // unmapped pour qu'il ne soit pas associé à une  entity property
-                'mapped' => false,
-
-                // pour le rendre optionnel
-                'required' => false,
-
-                // Contraintes de validations de fichier
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/png',
-                            'image/gif',
-                        ],
-                        'mimeTypesMessage' => 'Veuillez sélectionner une image valide',
-                    ])
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
                 ],
             ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Participants::class,
