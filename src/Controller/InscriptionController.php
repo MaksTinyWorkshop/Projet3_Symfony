d3c096bd699e47f77ad4b2238f1b15b5bee7cc43
Shim0ji <shimoji@hotmail.fr>
2024-05-30 17:08:43 +0200

    BIG MERGE !!!

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/inscription', name: 'inscription_')]
+class InscriptionController extends AbstractController
+{
+    #[Route('/add/{id}', name: 'add')]
+    public function add(): Response
+    {
+        return $this->render('inscription/index.html.twig', [
+            'controller_name' => 'InscriptionController',
+        ]);
+    }
+}
